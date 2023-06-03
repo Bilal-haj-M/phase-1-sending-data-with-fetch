8c720099cd83559b63394785bca98c7c875a2b99
Bilal-hajM <bblal1003@gmail.com>
2023-06-04 01:31:50 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+    const data = {
+      name: name,
+      email: email
+    };
+  
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch("http://localhost:3000/users", config)
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        document.body.innerHTML += `<p>id: ${id}</p>`;
+      })
+      .catch(error => {
+        document.body.innerHTML += `<p>${error.message}</p>`;
+      });
+  }
